f38f299d1a20bc2314b6cef67d7bf1318a8c5fda
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-09-29 16:03:31 -0600

    refresh button

--- app/src/components/FileExplorer/FileExplorer.js
+++ app/src/components/FileExplorer/FileExplorer.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Header, Segment, Icon, Breadcrumb } from 'semantic-ui-react'
+import { Header, Segment, Icon, Breadcrumb, Button } from 'semantic-ui-react'
+import './FileExplorer.css'
 
 const FileExplorer = () => {
   let currentPath = []
+  let refreshing
 
   return (
     <div>
       <Header as='h2'>
         <u>Files</u>
       </Header>
+      <div className='buttons'>
+        <Button basic color='green' size='mini'>
+          <Icon name='refresh' loading={refreshing}/>
+          { refreshing ? 'Loading...' : 'Refresh'}
+        </Button>
+      </div>
       <Breadcrumb>
         <Icon name='folder open outline'/>
         <Breadcrumb.Section link active={!currentPath.length}>Objects</Breadcrumb.Section>
