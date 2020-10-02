80593566dd7808f5e106e4975488eec71fa23b40
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-10-02 10:33:03 -0600

    remove config again

--- app/src/config.js
+++
@@ -1,6 +0,0 @@
-// Duplicate and remove the underscore from this file and add your google oauth token below before building
-
-export default {
-  googleClientId: '862168466162-7aadsscqo3k0bskalrrv896uapglqmrf.apps.googleusercontent.com',
-  APIEndpoint: 'https://us-central1-projects-290904.cloudfunctions.net'
-}
